f50aac2a32779e867befb8983e4909e7fcb563aa
PrataCat <111517381+PrataCat@users.noreply.github.com>
2022-11-25 20:36:02 +0000

    Deploying to gh-pages from @ PrataCat/test@22ad1e16b552cef01d69b4ad577787bf0c0b69ca 🚀

---
+++ take-order.a7d67474.js
@@ -0,0 +1,2 @@
+!function(){var e={body:document.querySelector("body"),mobMenuBtn:document.querySelector("[data-menu-btn]"),menu:document.querySelector("[data-menu]"),svgBurgerIcon:document.querySelector(".icon-open-btn"),svgCrossIcon:document.querySelector(".icon-close-btn")};e.mobMenuBtn.addEventListener("click",(function(){e.svgBurgerIcon.classList.toggle("is-open"),e.svgCrossIcon.classList.toggle("is-open"),e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=take-order.a7d67474.js.map
